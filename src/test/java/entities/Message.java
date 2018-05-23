6b71f65b97c4932d0043bf87f24d3b31b344ad6a
Artem Khoruzhiy <artem_juventini@mail.ru>
2018-05-23 18:52:20 +0300

    5th Changes

--- src/test/java/entities/Message.java
+++ src/test/java/entities/Message.java
@@ -16,23 +16,11 @@ public class Message {
         return addressee;
     }
 
-    public void setAddressee(String addressee) {
-        this.addressee = addressee;
-    }
-
     public String getSubject() {
         return subject;
     }
 
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 }
